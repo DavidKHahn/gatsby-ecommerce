6bd78103bcf4b74e0dbc1a3c154c9b559a3b6064
David <dhahn123@gmail.com>
2019-11-09 15:51:45 -0800

    display shopping cart summary in header

--- src/components/header.js
+++ src/components/header.js
@@ -13,6 +13,9 @@ const Header = ({ siteTitle }) => (
   >
     <div
       style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
@@ -43,6 +46,15 @@ const Header = ({ siteTitle }) => (
         </Link>
       </h1>
     </span>
+
+    {/* Shopping Cart Summary */}
+    <div style={{ color: 'white', cursor: 'pointer' }}className="snipcart-summary snipcart-checkout">
+      <div><strong>My Cart</strong></div>
+      <div><span style={{ fontWeight: 'bold' }} className="snipcart-total-items"></span>{" "}Items in Cart</div>
+      <div>Total Price{' '}<span
+      style={{ fontWeight: 'bold' }}
+      className="snipcart-total-price"></span></div>
+    </div>
     </div>
   </header>
 )
