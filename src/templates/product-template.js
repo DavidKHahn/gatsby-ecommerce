ce04da2ffde915f5b3d336cdcfaf961118df280f
David <dhahn123@gmail.com>
2019-11-09 15:35:18 -0800

    set up gatsby-plugin-snipcart: ability to purchase products

--- src/templates/product-template.js
+++ src/templates/product-template.js
@@ -4,7 +4,7 @@ import React from 'react'
 import Layout from '../components/layout'
 
 
-const ProductTemplate = ({ data: { contentfulProduct } }) => (
+const ProductTemplate = ({ data: { contentfulProduct }, location }) => (
     <Layout>
         <div
             style={{
@@ -16,7 +16,18 @@ const ProductTemplate = ({ data: { contentfulProduct } }) => (
             {/* Product Info */}
             <h2>{contentfulProduct.name} - <span style={{ color: '#ccc' }}>
             Added on {contentfulProduct.createdAt}</span></h2>
+            <h4>${contentfulProduct.price}</h4>
             <p>{contentfulProduct.description}</p>
+            <button
+                className="snipcart-add-item"
+                data-item-id={contentfulProduct.slug}
+                data-item-price={contentfulProduct.price}
+                data-item-image={contentfulProduct.image.file.url}
+                data-item-name={contentfulProduct.name}
+                data-item-url={location.pathname}
+            >
+            Add to Cart
+            </button>
             <Img style={{ margin: '0 auto', maxWidth: 600 }} fluid={contentfulProduct.image.fluid} />
         </div>
     </Layout>
@@ -25,6 +36,7 @@ const ProductTemplate = ({ data: { contentfulProduct } }) => (
 export const query = graphql`
 query($slug: String!) {
     contentfulProduct(slug: {eq: $slug }) {
+      slug
       name
       price
       description
@@ -32,6 +44,9 @@ query($slug: String!) {
       image {
           fluid(maxWidth: 800) {
               ...GatsbyContentfulFluid
+          }
+          file {
+              url
           }
       }
     }
