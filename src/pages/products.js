8ecec5f847e80e2e96d18b571652854fbf27f455
David <dhahn123@gmail.com>
2019-11-09 17:23:23 -0800

    Add private products feature to auth users

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,16 +1,40 @@
 import { graphql, Link } from 'gatsby'
 import Img from 'gatsby-image'
+import netlifyIdentity from 'netlify-identity-widget'
 import React from 'react'
 import Layout from '../components/layout'
 
 
-const Products = ({ data: { allContentfulProduct }}) => (
+class Products extends React.Component {
+    state = {
+        products: []
+    }
+
+    componentDidMount(){
+        this.getProducts()
+        netlifyIdentity.on('login', user => this.getProducts(user))
+        netlifyIdentity.on('logout', () => this.getProducts())
+    }
+
+    getProducts = user => {
+        console.log('Current User', user)
+        const allProducts = this.props.data.allContentfulProduct.edges
+        const products = netlifyIdentity.currentUser() !== null ?
+        allProducts :
+        allProducts.filter(({ node: product }) => !product.private)
+        this.setState({ products })
+    }
+
+    render(){
+        const { products } = this.state
+
+    return (
     <Layout>
         <div>
             {/* Products List */}
-            {allContentfulProduct.edges.map(({ node: product }) => (
+            {products.map(({ node: product }) => (
                 <div key={product.id}>
-                    <h2>Coffee Products</h2>
+                    <h2>Must Have Items</h2>
                     <Link to={`/products/${product.slug}`} style={{
                         textDecoration: 'none', color: '#551a8b'
                     }}>
@@ -28,7 +52,7 @@ const Products = ({ data: { allContentfulProduct }}) => (
             ))}
         </div>
     </Layout>
-)
+)}}
 
 export const query = graphql`
 {
@@ -39,6 +63,7 @@ export const query = graphql`
                 slug
                 name
                 price
+                private
                 image {
                     fluid(maxWidth: 400) {
                         ...GatsbyContentfulFluid_tracedSVG
